c5edd76b5de84a1be9a6479f2cec23a7d0682610
Filip Procházka <filip@prochazka.su>
2016-05-03 13:32:09 +0200

    add gulp and prepare webpack loaders

---
+++ client/gulpfile.babel.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef, no-console */
+import bg from 'gulp-bg';
+import del from 'del';
+import gulp from 'gulp';
+import webpackBuild from './webpack/build';
+
+gulp.task('clean', () => del('static/*'));
+
+gulp.task('build-webpack', ['clean'], webpackBuild);
+
+gulp.task('server-hot', bg('node', './webpack/server'));
+
+// Default task to start development. Just type gulp.
+gulp.task('default', ['server-hot']);
---
+++ client/webpack/makeConfig.js
@@ -0,0 +1,130 @@
+import autoprefixer from 'autoprefixer';
+import constants from './constants';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
+
+// cheap-module-eval-source-map, because we want original source, but we don't
+// care about columns, which makes this devtool faster than eval-source-map.
+// http://webpack.github.io/docs/configuration.html#devtool
+var devtools = 'cheap-module-eval-source-map';
+
+export default function makeConfig(isDevelopment) {
+	function stylesLoaders(loaders) {
+		return Object.keys(loaders).map(ext => {
+			const prefix = 'css-loader!postcss-loader';
+			const extLoaders = prefix + loaders[ext];
+			const loader = isDevelopment
+				? `style-loader!${extLoaders}`
+				: ExtractTextPlugin.extract('style-loader', extLoaders);
+			return {
+				loader,
+				test: new RegExp(`\\.(${ext})$`)
+			};
+		});
+	}
+
+	const config = {
+		cache: isDevelopment,
+		debug: isDevelopment,
+		devtool: isDevelopment ? devtools : '',
+		entry: {
+			app: isDevelopment ? [
+				'webpack-hot-middleware/client',
+				'./index'
+			] : [
+				'./index'
+			]
+		},
+		output: {
+			path: path.join(constants.ABSOLUTE_BASE, 'static'),
+			filename: 'bundle.js',
+			publicPath: '/static/'
+		},
+		plugins: (() => {
+			const plugins = [
+				new webpack.DefinePlugin({
+					'process.env': {
+						IS_BROWSER: true, // Because webpack is used only for browser code.
+						IS_SERVERLESS: JSON.stringify(process.env.IS_SERVERLESS || false),
+						NODE_ENV: JSON.stringify(isDevelopment ? 'development' : 'production'),
+						SERVER_URL: JSON.stringify(process.env.SERVER_URL || '')
+					}
+				})
+			];
+
+			if (isDevelopment) {
+				plugins.push(
+					new webpack.optimize.OccurenceOrderPlugin(),
+					new webpack.HotModuleReplacementPlugin(),
+					new webpack.NoErrorsPlugin()
+				);
+
+			} else {
+				plugins.push(
+					// Render styles into separate cacheable file to prevent FOUC and
+					// optimize for critical rendering path.
+					new ExtractTextPlugin('style.css', {
+						allChunks: true
+					}),
+					new webpack.optimize.DedupePlugin(),
+					new webpack.optimize.OccurenceOrderPlugin(),
+					new webpack.optimize.UglifyJsPlugin({
+						compress: {
+							screw_ie8: true, // eslint-disable-line camelcase
+							warnings: false // Because uglify reports irrelevant warnings.
+						}
+					})
+				);
+			}
+
+			return plugins;
+		})(),
+		postcss: () => [autoprefixer({browsers: 'last 2 version'})],
+		module: {
+			loaders: [
+				{
+					test: /\.jsx?$/,
+					loader: 'babel',
+					exclude: /node_modules/,
+					include: constants.ABSOLUTE_BASE,
+					query: {
+						cacheDirectory: true,
+						plugins: ['transform-runtime', 'add-module-exports'],
+						presets: ['es2015', 'react', 'stage-1'],
+						env: {
+							development: {
+								presets: ['react-hmre']
+							},
+							production: {
+								plugins: [
+									'transform-react-constant-elements',
+									'transform-react-inline-elements'
+								]
+							}
+						}
+					}
+
+				},
+				{
+					loader: 'url-loader?limit=10000',
+					test: /\.(gif|jpg|png|svg)$/
+				},
+				{
+					loader: 'url-loader?limit=1',
+					test: /favicon\.ico$/
+				},
+				{
+					loader: 'url-loader?limit=100000',
+					test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/
+				}
+			].concat(stylesLoaders({
+				css: '',
+				scss: '!sass-loader',
+				sass: '!sass-loader?indentedSyntax'
+			}))
+		}
+	};
+
+	return config;
+};
