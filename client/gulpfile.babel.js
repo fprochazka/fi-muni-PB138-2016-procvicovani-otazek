e6a85415ca320658be5cde67d27f46c5fc59d179
Filip Procházka <filip@prochazka.su>
2016-05-03 21:12:11 +0200

    reorganizce project structure

--- client/gulpfile.babel.js
+++ client/gulpfile.babel.js
@@ -2,13 +2,13 @@
 import bg from 'gulp-bg';
 import del from 'del';
 import gulp from 'gulp';
-import webpackBuild from './webpack/build';
+import webpackBuild from './src/webpack/build';
 
 gulp.task('clean', () => del('static/*'));
 
 gulp.task('build-webpack', ['clean'], webpackBuild);
 
-gulp.task('server-hot', bg('node', './webpack/server'));
+gulp.task('server-hot', bg('node', './src/webpack/server'));
 
 // Default task to start development. Just type gulp.
 gulp.task('default', ['server-hot']);
