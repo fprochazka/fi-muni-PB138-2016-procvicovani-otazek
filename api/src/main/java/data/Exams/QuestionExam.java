ee586906b4d883944a8e01efd8201303e8341474
Michaela Bamburova <michaela.bamburova@gmail.com>
2016-05-13 01:45:52 +0200

    generated equals, hashcode

--- api/src/main/java/data/Exams/QuestionExam.java
+++ api/src/main/java/data/Exams/QuestionExam.java
@@ -35,4 +35,25 @@ public class QuestionExam {
 			", numWrong=" + numWrong +
 			'}';
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		QuestionExam that = (QuestionExam) o;
+
+		if (numCorrect != that.numCorrect) return false;
+		if (numWrong != that.numWrong) return false;
+		return questionExamId != null ? questionExamId.equals(that.questionExamId) : that.questionExamId == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = questionExamId != null ? questionExamId.hashCode() : 0;
+		result = 31 * result + numCorrect;
+		result = 31 * result + numWrong;
+		return result;
+	}
 }
--- api/src/main/java/data/Exams/StudentExam.java
+++ api/src/main/java/data/Exams/StudentExam.java
@@ -23,4 +23,31 @@ public class StudentExam {
 		return DrillExams;
 	}
 
+	@Override
+	public String toString() {
+		return "StudentExam{" +
+			"studentExamId='" + studentExamId + '\'' +
+			", DrillExams=" + DrillExams +
+			'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		StudentExam that = (StudentExam) o;
+
+		if (studentExamId != null ? !studentExamId.equals(that.studentExamId) : that.studentExamId != null)
+			return false;
+		return DrillExams != null ? DrillExams.equals(that.DrillExams) : that.DrillExams == null;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = studentExamId != null ? studentExamId.hashCode() : 0;
+		result = 31 * result + (DrillExams != null ? DrillExams.hashCode() : 0);
+		return result;
+	}
 }
