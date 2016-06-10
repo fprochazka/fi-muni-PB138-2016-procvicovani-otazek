1999f406ec4cf5116234c9df145423f63a67bbea
Filip Procházka <filip@prochazka.su>
2016-06-10 20:53:39 +0200

    api: mongo tests for testing mongo

--- api/src/test/java/com/fprochazka/drill/Pb138drillApplicationTests.java
+++ api/src/test/java/com/fprochazka/drill/Pb138drillApplicationTests.java
@@ -12,13 +12,15 @@ import com.fprochazka.drill.model.exam.question.ExamQuestion;
 import com.fprochazka.drill.model.exam.question.ExamQuestionRepository;
 import com.fprochazka.drill.model.student.Student;
 import com.fprochazka.drill.model.student.StudentRepository;
+import com.mongodb.DB;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,72 +29,89 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ApplicationConfig.class)
-@WebAppConfiguration
-public class Pb138drillApplicationTests {
-
+public class Pb138drillApplicationTests
+{
 
 	@Autowired
 	QuestionRepository questionRepository;
+
+	@Autowired
 	StudentRepository studentRepository;
+
+	@Autowired
 	DrillRepository drillRepository;
+
+	@Autowired
 	ExamQuestionRepository examQuestionRepository;
+
+	@Autowired
 	ExamRepository examRepository;
 
-	@Before
-	public void init(){
-		questionRepository.save(createQuestion());
-		studentRepository.save(createStudent());
-		drillRepository.save(createDrill());
-		examQuestionRepository.save(createExamQuestion());
-		examRepository.save(createExam());
+	@Autowired
+	MongoTemplate mongoTemplate;
 
+	@Before
+	public void clearDatabase()
+	{
+		DB db = mongoTemplate.getDb();
+		db.getCollectionNames()
+			.stream()
+			.filter(collectionName -> !collectionName.startsWith("system."))
+			.forEach(collectionName -> {
+				mongoTemplate.remove(new Query(), collectionName);
+			});
 	}
 
-	public Question createQuestion() {
+	@Test
+	public void testFindOne()
+	{
+		createAndPersistFixtures();
 
-		String title = "New question";
+		List<Question> question = (List<Question>) questionRepository.findAll();
+		assertEquals(3, question.size());
+	}
+
+	@Test
+	public void testFindDrill()
+	{
+		Drill drill = createAndPersistFixtures();
+
+		List<Question> questions = (List<Question>) questionRepository.findAll();
+		assertEquals(questions, questionRepository.getQuestionsByDrill(drill.getId()));
+	}
+
+	private Drill createAndPersistFixtures()
+	{
+		Student student = new Student(123456, "mail@mail.com", "hash");
+		studentRepository.save(student);
+
+		Drill drill = new Drill("drill 1");
+		drillRepository.save(drill);
+
+		Question question1 = createQuestion(drill, "Question 1");
+		questionRepository.save(question1);
+		Question question2 = createQuestion(drill, "Question 1");
+		questionRepository.save(question2);
+		Question question3 = createQuestion(drill, "Question 1");
+		questionRepository.save(question3);
+
+		Exam exam = new Exam(drill, student);
+		examRepository.save(exam);
+		ExamQuestion examQuestion1 = new ExamQuestion(question1, exam, 2, 3);
+		examQuestionRepository.save(examQuestion1);
+		ExamQuestion examQuestion2 = new ExamQuestion(question2, exam, 1, 0);
+		examQuestionRepository.save(examQuestion2);
+
+		return drill;
+	}
+
+	private Question createQuestion(Drill drill, String title)
+	{
 		List<Answer> answers = new ArrayList<>();
 		answers.add(new Answer(true, "this is right"));
 		answers.add(new Answer(false, "this is wrong"));
-		Drill drill = createDrill();
 
 		return new Question(title, answers, drill);
 	}
 
-	public Drill createDrill() {
-		return new Drill("drill 1");
-	}
-
-	public ExamQuestion createExamQuestion() {
-		Question question = createQuestion();
-		Exam exam = createExam();
-		int correct = 2;
-		int wrong = 3;
-		return new ExamQuestion(question, exam, correct, wrong);
-	}
-
-	public Student createStudent() {
-		return new Student(123456, "mail@mail.com", "hash");
-	}
-
-	public Exam createExam() {
-		Drill drill = createDrill();
-		Student student = createStudent();
-		return new Exam(drill, student);
-	}
-
-
-
-
-	@Test
-	public void testFindOne() {
-		List<Question> question = (List<Question>) questionRepository.findAll();
-		assertEquals(1, question.size());
-	}
-
-	@Test
-	public void testFindDrill() {
-		List<Question> questions = (List<Question>) questionRepository.findAll();
-		assertEquals(questions, questionRepository.getQuestionsByDrill(createDrill().getId()));
-	}
 }
