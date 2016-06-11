a8db4ffd186817e86a6bd2f3c13db4808ab9c009
VikiVozarova <vozarova.viki@gmail.com>
2016-06-11 12:31:22 +0200

    Uco set to Integer, added NotNull annotation. Drill controller touch up.

--- api/src/main/java/com/fprochazka/drill/api/student/CreateStudentRequest.java
+++ api/src/main/java/com/fprochazka/drill/api/student/CreateStudentRequest.java
@@ -3,22 +3,23 @@ package com.fprochazka.drill.api.student;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by viki on 11.06.16.
  */
 public class CreateStudentRequest
 {
-	@NotEmpty
-	private int uco;
+	@NotNull
+	private Integer uco;
 
 	@NotEmpty
 	private String email;
 
 	@NotEmpty
-	@Email
 	private String password;
 
-	public CreateStudentRequest(int uco, String email, String password)
+	public CreateStudentRequest(Integer uco, String email, String password)
 	{
 		this.uco = uco;
 		this.email = email;
@@ -29,7 +30,7 @@ public class CreateStudentRequest
 	{
 	}
 
-	public int getUco()
+	public Integer getUco()
 	{
 		return uco;
 	}
